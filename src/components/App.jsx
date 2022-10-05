4f159675e922444bc7d59e04929fe25af9032e7b
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-05 21:34:02 +0300

    add files and start render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { Component } from 'react';
+import { FeedBack } from './Feedback/FeedBack';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <FeedBack />
     </div>
   );
 };
