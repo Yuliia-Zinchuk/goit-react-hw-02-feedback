a7d23499189c9db131f2e8b8bf0de5a97bb6accf
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-08 17:52:35 +0300

    Update FeedBack.jsx

--- src/components/Feedback/FeedBack.jsx
+++ src/components/Feedback/FeedBack.jsx
@@ -1,28 +1,57 @@
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import css from './FeedBack.module.css';
-export const FeedBack = ({ id, good, neutral, bad }) => {
-  return (
-    <div className={css.card}>
-      <h1 className={css.title}>Please leave feedback</h1>
-      <div className={css.btn_list}>
-        <button className={css.btn}>Good</button>
-        <button className={css.btn}>Neutral</button>
-        <button className={css.btn}>Bad</button>
+class FeedBack extends Component {
+  render() {
+    const { id, good, neutral, bad } = this.props;
+    return (
+      <div className={css.card}>
+        <h1 className={css.title}>Please leave feedback</h1>
+        <div className={css.btn_list}>
+          {/* <button className={css.btn}>Good</button> */}
+          <button className={css.btn}>Neutral</button>
+          <button className={css.btn}>Bad</button>
+        </div>
+        <h2 className={css.title_list}>Statistics</h2>
+        <ul className={css.stat_list}>
+          <li className={css.item}>
+            <span className={css.label}>Good: {good}</span>
+            {/* <span className={css.count}></span> */}
+          </li>
+          <li className={css.item}>
+            <span className={css.label}>Neutral: </span>
+          </li>
+          <li className={css.item}>
+            <span className={css.label}>Bad: </span>
+          </li>
+        </ul>
       </div>
-      <h2 className={css.title_list}>Statistics</h2>
-      <ul className={css.stat_list}>
-        <li className={css.item}>
-          <span className={css.label}>Good: </span>
-          {/* <span className={css.count}></span> */}
-        </li>
-        <li className={css.item}>
-          <span className={css.label}>Neutral: </span>
-        </li>
-        <li className={css.item}>
-          <span className={css.label}>Bad: </span>
-        </li>
-      </ul>
-    </div>
-  );
-};
+    );
+  }
+}
+export default FeedBack;
+//  export const FeedBack = ({ id, good, neutral, bad }) => {
+//   return (
+//     <div className={css.card}>
+//       <h1 className={css.title}>Please leave feedback</h1>
+//       <div className={css.btn_list}>
+//         <button className={css.btn}>Good</button>
+//         <button className={css.btn}>Neutral</button>
+//         <button className={css.btn}>Bad</button>
+//       </div>
+//       <h2 className={css.title_list}>Statistics</h2>
+//       <ul className={css.stat_list}>
+//         <li className={css.item}>
+//           <span className={css.label}>Good: </span>
+//           {/* <span className={css.count}></span> */}
+//         </li>
+//         <li className={css.item}>
+//           <span className={css.label}>Neutral: </span>
+//         </li>
+//         <li className={css.item}>
+//           <span className={css.label}>Bad: </span>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
